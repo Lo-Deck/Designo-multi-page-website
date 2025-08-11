2ffa853ad4ea26bcdddfb60bca81c9f7ba387d76
Lo-Deck <122597745+Lo-Deck@users.noreply.github.com>
2025-08-11 08:55:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,251 @@
+
+/* OPEN CLOSE MENU */
+
+const btnMenu = document.querySelector('.button-menu');
+
+let isMenuClicked = false;
+
+btnMenu.addEventListener('click', () => {
+
+    isMenuClicked = !isMenuClicked;
+
+    if(isMenuClicked){
+        document.querySelector('.menu').style.display = 'block';
+        document.querySelector('.background-menu').style.display = 'block';
+        document.querySelector('.button-menu img').src = './assets/shared/mobile/icon-close.svg';
+    }
+    else{
+        document.querySelector('.menu').style.display = 'none';
+        document.querySelector('.background-menu').style.display = 'none';
+        document.querySelector('.button-menu img').src = './assets/shared/mobile/icon-hamburger.svg';
+    }
+
+});
+
+
+
+/* FORM */
+
+if(document.querySelector('.contact')){
+
+    const myForm = document.querySelector('#myForm');
+    const formInputs = myForm.querySelectorAll('.input, textarea'); 
+
+    formInputs.forEach( (input, index) => {
+
+        input.addEventListener('input', () => {
+            document.querySelectorAll('.container-input span.error')[index].textContent = '';
+            document.querySelectorAll('.container-input span.error')[index].classList.remove('logo-error');
+        });
+
+    });
+
+
+    function validateInput(input, index){
+
+        if (input.validity.valueMissing) {
+            document.querySelectorAll('.container-input span.error')[index].textContent = `Can't be empty`;
+            document.querySelectorAll('.container-input span.error')[index].classList.add('logo-error');
+            return false;
+        }
+
+        if (input.validity.tooShort) {
+            document.querySelectorAll('.container-input span.error')[index].textContent = `Input too short`;
+            document.querySelectorAll('.container-input span.error')[index].classList.add('logo-error');
+            return false;
+        }
+
+        if (input.validity.tooLong) {
+            document.querySelectorAll('.container-input span.error')[index].textContent = `Input too long`;
+            document.querySelectorAll('.container-input span.error')[index].classList.add('logo-error');
+            return false;
+        }
+
+        if (input.type === 'email' && input.validity.typeMismatch) {
+            document.querySelectorAll('.container-input span.error')[index].textContent = `Not a valid email address`;
+            document.querySelectorAll('.container-input span.error')[index].classList.add('logo-error');
+            return false;
+        }
+
+        if (input.type === 'tel' && input.validity.patternMismatch ) {
+            document.querySelectorAll('.container-input span.error')[index].textContent = `Not a phone number`;
+            document.querySelectorAll('.container-input span.error')[index].classList.add('logo-error');
+            return false;
+        }
+
+        document.querySelectorAll('.container-input span.error')[index].textContent = '';
+        document.querySelectorAll('.container-input span.error')[index].classList.remove('logo-error');
+        return true;
+
+    }  
+
+    
+    myForm.addEventListener('submit', (event) => {
+
+        formInputs.forEach( (input, index) => {
+
+            if (!validateInput(input, index)){
+                event.preventDefault(); 
+            }
+            else{
+                console.log('form submit');
+                event.preventDefault(); 
+            }
+
+        });
+
+    });
+
+
+}
+
+
+
+/* MAP */
+
+if(document.querySelector('.locations')){
+
+    
+
+    const mapCanada = L.map('map-canada', { scrollWheelZoom: false }).setView([43.64397, -79.38190], 11);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(mapCanada);  
+
+
+    const CanadaOffice = L.marker([43.64740, -79.38157]).addTo(mapCanada);
+
+    CanadaOffice.bindTooltip(
+        `<b>Designo Central Office</b><br>3886 Wellington Street`,
+        {
+            permanent: false,
+            direction: 'top',
+            offset: [0, -10]
+        }
+    );
+
+
+
+    const mapAustralia = L.map('map-australia', { scrollWheelZoom: false }).setView([-27.6437, 153.0220], 9);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(mapAustralia); 
+
+    const AustraliaOffice = L.marker([-27.6437, 153.0220]).addTo(mapAustralia);
+
+    AustraliaOffice.bindTooltip(
+        `<b>Designo AU Office</b><br>19 Balonne Street`,
+        {
+            permanent: false,
+            direction: 'top',
+            offset: [0, -10]
+        }
+    );
+
+
+
+    const mapUK = L.map('map-UK', { scrollWheelZoom: false }).setView([51.5243, -0.1013], 11);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(mapUK); 
+
+    const UKOffice = L.marker([51.5243, -0.1013]).addTo(mapUK);
+
+    UKOffice.bindTooltip(
+        `<b>Designo UK Office</b><br>13 Colorado Way`,
+        {
+            permanent: false,
+            direction: 'top',
+            offset: [0, -10]
+        }
+    );
+
+
+    //map zoom
+
+    [CanadaOffice, AustraliaOffice, UKOffice].forEach((item, index) => {
+
+        item.on("click", () => {
+
+            if(index === 0){
+               mapCanada.setView(item.getLatLng(), 16);
+            }
+
+            if(index === 1){
+               mapAustralia.setView(item.getLatLng(), 16);
+            }
+
+            if(index === 2){
+               mapUK.setView(item.getLatLng(), 16);
+            }
+        
+        })
+
+    });
+
+
+
+
+    if(window.innerWidth >= 768){
+
+        const mapContainer = document.querySelectorAll('.image-map');
+        const textOnMouseMove = document.querySelector(".text-mouse-map-hover");
+
+        mapContainer.forEach( (map, index) => {
+
+            map.addEventListener('mousemove', (event) => {
+
+                textOnMouseMove.style.display = 'inline-block';
+                textOnMouseMove.style.left = event.clientX + 'px';
+                textOnMouseMove.style.top = event.clientY + 'px';
+
+            });            
+
+        });
+
+
+        mapContainer.forEach( (map, index) => {
+
+            map.addEventListener('mouseout', () => {
+                textOnMouseMove.style.display = 'none';
+            });
+
+        });
+
+
+        document.addEventListener('keydown', (event) => {
+
+            if (event.key === 'Control') { 
+
+                [mapCanada, mapAustralia, mapUK].forEach( (map, index) => {
+                    map.scrollWheelZoom.enable();    
+                });
+        
+            }
+
+        });
+
+
+        document.addEventListener('keyup', (event) => {
+
+            if (event.key === 'Control') { 
+
+                [mapCanada, mapAustralia, mapUK].forEach( (map, index) => {
+                    map.scrollWheelZoom.disable();
+                });
+
+            }
+
+        });
+
+
+    }
+
+
+}
